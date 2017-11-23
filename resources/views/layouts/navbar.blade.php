0beada769225b42a7255c84b8271175997d734c1
Geovanny Juárez <geovanny.juarez@outlook.com>
2017-11-22 18:32:57 -0600

    AjustesNavbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,12 +6,12 @@
   <!-- Navigation. We hide it in small screens. -->
   <nav class="mdl-navigation mdl-layout--large-screen-only">
     <!--Enlaces comunes-->
-    <a class="mdl-navigation__link" href="">Inicio</a>
-    <a class="mdl-navigation__link" href="">Nosotros</a>
-    <a class="mdl-navigation__link" href="">Ubicacion</a>
-    <a class="mdl-navigation__link" href="">Blog</a>
-    <a class="mdl-navigation__link" href="Asistencia">Eventos</a>
+    <a class="mdl-navigation__link" href="/">Inicio</a>
+    <a class="mdl-navigation__link" href="/">Nosotros</a>
+    <a class="mdl-navigation__link" href="/">Ubicacion</a>
+    <a class="mdl-navigation__link" href="/">Blog</a>
+    <a class="mdl-navigation__link" href="/">Eventos</a>
     <!--Enlaces administracion-->
-    <a class="mdl-navigation__link" href="">Admin</a>
+    <a class="mdl-navigation__link" href="Asistencia" id="nav-asistencia">Admin</a>
   </nav>
 </div>
